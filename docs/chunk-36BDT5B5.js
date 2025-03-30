196257520144838f46bd60fdb2110a4ba33876d0
Mohamed <mohamedkhlifadd@gmail.com>
2025-03-30 17:48:37 +0200

    update host

---
+++ docs/chunk-36BDT5B5.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-SXJCKCVU.js").then(o=>o.HomePageComponent)}];export{t as homeRoutes};
--- docs/chunk-QAJ5EY6I.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-H672ZY5Z.js").then(o=>o.HomePageComponent)}];export{t as homeRoutes};
